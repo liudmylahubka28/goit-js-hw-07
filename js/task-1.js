e45bb0e9ae3792735836a2bd2b9d78a76918ecd0
Liudmyla Hubka <liudmylahubka28@gmail.com>
2026-02-26 16:24:39 +0100

    add my seventh js-hw correct

--- js/task-1.js
+++ js/task-1.js
@@ -2,7 +2,7 @@ const categoriesList = document.querySelector("#categories");
 
 const categoriesItems = categoriesList.querySelectorAll(".item");
 
-console.log(`Number of categories: ${categoriesItems.lemgth}`);
+console.log(`Number of categories: ${categoriesItems.length}`);
 
 categoriesItems.forEach(item => {
     const title = item.querySelector("h2").textContent;
